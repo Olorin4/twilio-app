98fe11691b7310822d81e0c45d15dd6ee5a74b3b
Nick Kalas <nick.kalas@proton.me>
2025-10-12 21:08:06 +0300

    fix(db): align database connection with deployment script

--- src/logManager.js
+++ src/logManager.js
@@ -8,11 +8,7 @@ const { Pool } = require("pg");
 
 // Configure PostgreSQL connection
 const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASS,
-  port: process.env.DB_PORT,
+  connectionString: process.env.DB_URL,
 });
 
 const client = twilio(accountSid, authToken);
